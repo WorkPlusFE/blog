b2a35bc71036f2b945ab34a61ab988036f900f12
Hejx <531601727@qq.com>
2021-11-08 13:15:18 +0800

    refactor: 优化“关于”页面的文案

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="flex flex-col md:flex-row items-center pt-4 mb-12">
-    <div class="relative w-30 h-30 rounded-1 overflow-hidden flex-shrink-0 md:mr-10 lt-md:mb-10">
-      <img src="/avatar.png" alt="Han" class="absolute w-full h-full object-cover !m-0">
+  <div class="flex pt-4 mb-12 flex-col">
+    <div class="flex flex-col md:flex-row">
+      <div class="relative w-18 h-18 rounded-1 overflow-hidden flex-shrink-0 md:mr-4 lt-md:mb-4">
+        <img src="/avatar.png" alt="Han" class="absolute w-full h-full object-cover !m-0">
+      </div>
+      <div>
+        <h1 class="text-2xl font-extrabold mb-2">
+          WorkPlusFE
+        </h1>
+        <p class="leading-[1.75]">
+          <a href="https://www.workplus.io/h-col-102.html" target="_blank" rel="noopener noreferrer">深圳恒拓高科信息技术有限公司</a> - 前端技术部
+        </p>
+      </div>
     </div>
-    <div>
-      <h1 class="text-4xl font-extrabold mb-4">
-        WorkPlusFE
-      </h1>
-      <p class="leading-[1.75]" />
-    </div>
+    <p class="leading-[1.75] mt-4">
+      任务主要围绕自研的 WorkPlus 移动办公平台开展，包括内嵌到原生 App 内的 Hybrid H5、核心移动办公应用（考勤打卡、流程审批及通用表单等）、低代码平台、积分商城、中台、基于 Electron/NW 开发的桌面客户端，以及小程序等。
+    </p>
   </div>
 </template>
